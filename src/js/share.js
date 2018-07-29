6d338bcfb1071e3b6735e28565ba52af91d42b6b
Adam Duncan <mail@adamduncandesigns.com>
2018-07-29 18:24:57 +0100

    Condition for native/custom sharing, button styling

--- src/js/share.js
+++ src/js/share.js
@@ -9,17 +9,28 @@ function bindClickEvents () {
 
 // actions
 function share () {
-  if (!navigator.share) return
-
-  const localSelections = (window.localStorage) ? window.localStorage.getItem(storageUserId) || '' : ''
+  const localSelections = window.localStorage ? window.localStorage.getItem(storageUserId) || '' : ''
   const shareCode = window.btoa(localSelections)
   const sharePath = `https://arctangent.netlify.com/${fragmentPrefix}${shareCode}`
 
+  if (navigator.share !== undefined) {
+    shareNative(sharePath)
+    return
+  }
+
+  shareCustom(sharePath)
+}
+
+function shareNative (sharePath) {
   navigator.share({
     title: 'ArcTanGent 2018',
-    text: 'Check out my highlights at ArcTanGent 2018',
+    text: 'Check out my highlights at ArcTanGent 2018: ',
     url: sharePath
   })
+}
+
+function shareCustom (sharePath) {
+  console.log('shareCustom', sharePath)
 }
 
 function init () {
