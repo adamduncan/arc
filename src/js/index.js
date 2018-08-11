f6341d365795d79c9c0b81d9d179aa78dd04cbf8
Adam Duncan <mail@adamduncandesigns.com>
2018-08-11 18:01:53 +0100

    Sack off scroll events and piggyback flkty scroll event instead

--- src/js/index.js
+++ src/js/index.js
@@ -3,34 +3,18 @@ import schedule from './schedule'
 import share from './share'
 
 const rAFSupported = window.requestAnimationFrame
-let lastKnownScrollPos = 0
-let ticking = false
 
 const timetableEl = document.getElementById('timetable')
 const timetableTop = timetableEl ? timetableEl.offsetTop : 0
-let timetableHeaders
+const headersMirrorEl = document.querySelector('[data-headers-mirror]')
 
 // CAROUSEL
-const positionStickyHeader = scrollPos => {
-  const headerCount = timetableHeaders.length
-  if (scrollPos > timetableTop) {
-    const offsetDistance = scrollPos - timetableTop
-    for (let i = 0; i < headerCount; i++) {
-      timetableHeaders[i].style.transform = `translate3d(0, ${offsetDistance}px, 0)`
-    }
-  } else {
-    for (let j = 0; j < headerCount; j++) {
-      timetableHeaders[j].removeAttribute('style')
-    }
-  }
-}
-
 const scrollToTimetable = () => {
   if (!timetableEl || timetableTop === window.scrollY) return
   window.scroll({ 
     top: timetableTop + 1, // fix subpixel
-    left: 0
-    // behavior: 'smooth'
+    left: 0,
+    behavior: 'smooth'
   });
 }
 
@@ -39,12 +23,7 @@ const flkty = new Flickity('[data-carousel]', {
   prevNextButtons: false,
   pageDots: false,
   selectedAttraction: 0.2,
-  friction: 0.8,
-  on: {
-    ready: () =>{
-      timetableHeaders = document.querySelectorAll('.is-selected [data-timetable-header]')
-    }
-  }
+  friction: 0.8
 })
 
 const slideButtons = document.querySelectorAll('[data-carousel-button]');
@@ -56,33 +35,21 @@ slideButtons.forEach((slideButton, i) => {
   })
 })
 
-flkty.on('select', function() {
+flkty.on('select', () => {
   const previousSelectedButton = document.querySelector('[data-carousel-button].is-selected')
   const selectedButton = slideButtons[flkty.selectedIndex]
   if (previousSelectedButton) previousSelectedButton.classList.remove('is-selected')
   if (selectedButton) selectedButton.classList.add('is-selected')
 })
 
-flkty.on('change', function() {
+flkty.on('change', () => {
   if (!rAFSupported) return
-  timetableHeaders = document.querySelectorAll('.is-selected [data-timetable-header]')
   window.requestAnimationFrame(scrollToTimetable)
 })
 
-if (rAFSupported) {
-  window.addEventListener('scroll', (e) => {
-    lastKnownScrollPos = window.scrollY
-    if (!ticking) {
-      window.requestAnimationFrame(() => {
-        positionStickyHeader(lastKnownScrollPos)
-        // positionStickyHeader(window.scrollY)
-        ticking = false
-      })
-      ticking = true
-    }
-  })
-}
-
+flkty.on('scroll', (event, progress) => {
+  if (headersMirrorEl) headersMirrorEl.style.transform = `translateX(${ -progress }px)`
+})
 
 // SCHEDULE
 schedule.init()
