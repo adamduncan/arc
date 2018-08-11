8a7fd41ccf9946d14d659480e38f1fa653063974
Adam Duncan <mail@adamduncandesigns.com>
2018-08-11 10:26:18 +0100

    Testing Timetable carousel

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import Frtabs from './tabs'
+import Flickity from 'flickity'
 import schedule from './schedule'
 import share from './share'
 
@@ -6,7 +6,7 @@ const timetableEl = document.getElementById('timetable')
 const timetableTop = timetableEl ? timetableEl.offsetTop : 0
 const rAFSupported = window.requestAnimationFrame
 
-// TABS
+// CAROUSEL
 const scrollToTimetable = () => {
   if (!timetableEl || timetableTop === window.scrollY) return
   window.scroll({ 
@@ -16,11 +16,26 @@ const scrollToTimetable = () => {
   });
 }
 
-const eventTabs = Frtabs({
-  onShowTab: () => {
-    if (!rAFSupported) return
-    window.requestAnimationFrame(scrollToTimetable)
-  }
+const flkty = new Flickity('[data-carousel]', {
+  adaptiveHeight: true,
+  prevNextButtons: false,
+  pageDots: false
+})
+const slideButtons = document.querySelectorAll('[data-carousel-button]');
+slideButtons.forEach((slideButton, i) => {
+  slideButton.addEventListener('click', event => {
+    event.preventDefault()
+    flkty.select(i)
+  })
+})
+
+flkty.on('select', function() {
+  const previousSelectedButton = document.querySelector('[data-carousel-button].is-selected')
+  const selectedButton = slideButtons[flkty.selectedIndex]
+  if (previousSelectedButton) previousSelectedButton.classList.remove('is-selected')
+  if (selectedButton) selectedButton.classList.add('is-selected')
+  if (!rAFSupported) return
+  window.requestAnimationFrame(scrollToTimetable)
 })
 
 // SCHEDULE
